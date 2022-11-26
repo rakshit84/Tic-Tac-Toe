ba8358daf7b08215f7e7ecf4847e846b51de429f
rakshit84 <116369542+rakshit84@users.noreply.github.com>
2022-11-26 07:01:21 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Board from './components/Board'
+import './App.css'
+
+const App = () => {
+  return (
+    <>
+    <Board />
+    </>
+  )
+}
+
+export default App
